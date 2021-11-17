4bfd637cc39553d56940d8396ec2c4f2e8ede97c
Varun Sharma <varunsh@stepsecurity.io>
2021-11-16 20:07:42 -0800

    Update firewall_test.go

--- firewall_test.go
+++ firewall_test.go
@@ -23,4 +23,21 @@ func Test_addAuditRules(t *testing.T) {
 
 	ipt.ClearChain("filter", "OUTPUT")
 	ipt.ClearChain("filter", "DOCKER-USER")
+
+	endpoints := []ipAddressEndpoint{}
+	endpoints = append(endpoints, ipAddressEndpoint{ipAddress: "1.1.1.1", port: "443"})
+
+	err = addBlockRulesForGitHubHostedRunner(endpoints)
+	if err != nil {
+		t.Errorf("Error not expected %v", err)
+	}
+
+	ipt, err = iptables.New()
+
+	if err != nil {
+		t.Errorf("Error not expected creating iptables %v", err)
+	}
+
+	ipt.ClearChain("filter", "OUTPUT")
+	ipt.ClearChain("filter", "DOCKER-USER")
 }
