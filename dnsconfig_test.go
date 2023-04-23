2e72a9e21be7cab83b0d8dd0fdbca6d1eca9b85c
Varun Sharma <varunsh@stepsecurity.io>
2023-04-23 05:45:16 -0700

    Add fix to not run docker containers with root access (#376)

--- dnsconfig_test.go
+++ dnsconfig_test.go
@@ -41,11 +41,11 @@ func Test_updateDockerConfig(t *testing.T) {
 	}{
 		{name: "existing file",
 			args:    args{configPath: tmpFileName},
-			want:    "{\"cgroup-parent\":\"/actions_job\",\"dns\":[\"172.17.0.1\"],\"live-restore\":true}",
+			want:    "{\"cgroup-parent\":\"/actions_job\",\"dns\":[\"172.17.0.1\"],\"live-restore\":true,\"userns-remap\":\"runner:runner\"}",
 			wantErr: false},
 		{name: "non existent file",
 			args:    args{configPath: mockDockerConfigPath},
-			want:    "{\"dns\":[\"172.17.0.1\"],\"live-restore\":true}",
+			want:    "{\"dns\":[\"172.17.0.1\"],\"live-restore\":true,\"userns-remap\":\"runner:runner\"}",
 			wantErr: false},
 	}
 
