16de40de28d186ae843c17a65f6c83530da0a0f9
Rohan Prabhu <rohan@stepsecurity.io>
2025-05-02 19:39:02 +0530

    chore: print buildinfo

--- buildinfo.go
+++ buildinfo.go
@@ -1,6 +1,13 @@
 package main
 
+import "fmt"
+
 // filled through ldflags
 var (
 	ReleaseTag = ""
+	commit     = ""
 )
+
+func LogBuildInfo() {
+	WriteLog(fmt.Sprintf("[buildInfo] tag=%s commit=%s \n", ReleaseTag, commit))
+}
